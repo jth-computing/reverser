94a2eb0dc11999a2a1c13891eee0d9ca109be3cc
SJ <josa24ym@student.ju.se>
2026-04-13 13:59:57 +0200

    fixed bug and added a test case

--- src/reverselib.py
+++ src/reverselib.py
@@ -2,14 +2,8 @@
 
 
 def reverse(n):
-    """
-    This function reverses the digits of a given integer.
-
-    >>> reverse(234)
-    432
-    """
     r = 0
-    while n > 1:
+    while n >= 1:
         r = r * 10 + n % 10
         n = n // 10
     return r
--- tests/test_reverseno.py
+++ tests/test_reverseno.py
@@ -8,5 +8,8 @@ class TestReverse(unittest.TestCase):
     def test_ott(self):
         self.assertEqual(reverse(234), 432)
 
+    def test_big(self):
+        self.assertEqual(reverse(123456789), 987654321)
+
 if __name__ == '__main__':
     unittest.main()
