7211da2a07b7ec40d82b363d849c1bfb32357f67
Linus Jacobsson <jali24qy@student.ju.se>
2026-04-13 14:35:40 +0200

    Added 2 testcases

--- src/reverselib.py
+++ src/reverselib.py
@@ -7,9 +7,17 @@ def reverse(n):
 
     >>> reverse(234)
     432
+
+    this is a test for all numbers
+    >>> reverse(123456789)
+    987654321
+
+    this test does not work
+    >>> 54321
+    12344
     """
     r = 0
-    while n > 1:
+    while n > 0:
         r = r * 10 + n % 10
         n = n // 10
     return r
